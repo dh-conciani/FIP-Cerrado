d68bcea1bff2757611864d7fa3385f1fca5ae604
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2022-02-16 10:48:55 -0300

    Update and rename GCP_to_GEE.py to ingestGEE.py

--- ingestGEE.py
+++ ingestGEE.py
@@ -8,7 +8,7 @@ import pandas as pd
 from google.cloud import storage
 
 ## api credentials
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "dest-file-to-json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:/api_key_ipam.json"
 ee.Initialize()
 
 ## start client
@@ -36,31 +36,15 @@ for blob_i in gcp_file:
 ## remove .tif extenstion from basename
 for gee_file_i in basename:
     gee_file = gee_file + [gee_file_i.split(sep='.')[0]]    
-    
-    
-## start client
-client = storage.Client()
 
-## define bucket
-bucket_name = 'fip-data'
-asset_name = 'users/dh-conciani/basemaps/fip-inpe-2004'
-
-## create empty recipies
-gcp_file = []
-basename = []
-gee_file = []
-
-## parse all files into recipe and store in the recipe object 
-for blob in client.list_blobs(bucket_name):
-    gcp_file = gcp_file + [('gs://' + bucket_name + '/' + str(blob).split(sep=', ')[1])]  
+## perform batch upload
+for i, item in enumerate(gcp_file):
+    ## print progress
+    print('ingesting file: ' + gcp_file[i])
+    print('file ' + str(i) + ' of ' + str(len(gcp_file)) + ' (' + str(round((i/len(gcp_file) * 100))) + '%)')
     
-print (gcp_file)
-    
-## parse all complete recipe filenames and extract only the basename
-for blob_i in gcp_file:
-    basename = basename + [asset_name + '/' + blob_i.split(sep='/')[3]]
-    
-## remove .tif extenstion from basename
-for gee_file_i in basename:
-    gee_file = gee_file + [gee_file_i.split(sep='.')[0]]    
+    ## ingest file into GEE
+    !earthengine upload image --asset_id={gee_file[i]} {gcp_file[i]}
+    print('')
+print ('done :-)')
 
